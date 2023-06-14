a58144c2f10fd74f24a5249a3d3d5fc6715912a0
gabriel-duarte154 <127501966+gabriel-duarte154@users.noreply.github.com>
2023-06-14 12:11:01 -0300

    Making home, menu and contact pages

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("It's up to begin.");
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('It\'s up to begin.');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLGtDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1wYWdlLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdJdFxcJ3MgdXAgdG8gYmVnaW4uJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
